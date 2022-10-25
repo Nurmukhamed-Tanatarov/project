f6059d507fee3376128038ec9095b93f7ed8d8a3
Nurik <psmartzhuawei@gmail.com>
2022-10-25 12:22:47 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var number = 18;
+
+console.log(4/0);
+console.log('asdfg'/0);
+
+let chto = {
+    name: "Nurik",
+    age: 16,
+ismarried: false
+};
+console.log(chto.name);
+
+/*alert('hi, world');
+let answer = confirm("are you luve this");
+
+console.log(answer);*/
+
+let answer = prompt("whats your name?", "my name");
+
+console.log(answer);
+
+let nur = false,
+    ai = false;
+    console.log(nur || ai);
